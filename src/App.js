ab029cfd4aa0e9039d23622a4b306a208d000f44
DRVDA <drvdaemail@gmail.com>
2022-06-18 21:36:32 +0100

    attl

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Login from './view/login';
+import Dashboard from './view/dashboard';
+import ClientesList from './view/clientesList';
+
 import Form from './view/form';
 import List from './view/list';
 import Edit from './view/edit';
@@ -9,26 +13,14 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="https://i.pinimg.com/originals/01/63/7c/01637cb20c70eb06221e5aa47a196ebb.png"
-            style={{ color: 'red', fontWeight: 'bold' }}>OwO Filmes OvO</a>
-          <button className="navbar-toggler" type="button" datatoggle="collapse" data-target="#navbarSupportedContent" ariacontrols="navbarSupportedContent" aria-expanded="false" arialabel="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse"
-            id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/"> Lista de Filmes </Link>
-              </li>
-            </ul>
-            <Link className="btn btn-success " to="/form">Adicionar Filme</Link>
-          </div>
-        </nav>
+        
         <div className="container py-4">
           <div className="row">
             <Routes>
-              <Route path="/" element={<List />} />
+              <Route path="/" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/clientesList" element={<ClientesList />} />
+
               <Route path="/form" element={<Form />} />
               <Route path="/edit/:filmeid" element={<Edit />} />
             </Routes>
---
+++ src/view/login.js
@@ -0,0 +1,38 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "../css/style.css";
+
+import axios from "axios";
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+//sweetalert2 - importação
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
+
+
+export default function Login() {
+  return (
+      <div class="container-fluid">
+
+        {/* Parte Cima */}
+        <div class=" row border bg-light-pink p-5">
+
+        </div>
+
+        {/* Parte Baixo */}
+        <h4 class="col-12 d-flex justify-content-center mt-5 mb-4">Login</h4>
+        <div class="col-12 d-flex justify-content-center ">
+          <input type="text" class="col-3 m-2" placeholder="Email:" />
+        </div>
+        <div class="col-12 d-flex justify-content-center ">
+          <input type="text" class="col-3 m-2" placeholder="Passe:" />
+        </div>
+
+        <div class="col-12 d-flex justify-content-center mt-4">
+          <button type="button" class="button-caseiro bg-dark-pink text-light">
+            <b>Entrar</b>
+          </button>
+        </div>
+      </div>
+  );  
+}
